1587ec84760a98eefd064ee44eef2a043a0ec782
tito10047 <jozef@mostka.com>
2016-05-24 18:40:23 +0200

    first commit

---
+++ lib/PhpToJs/Autoloader.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jozef Môstka
+ * Date: 24.5.2016
+ * Time: 18:20
+ */
+
+namespace phptojs;
+
+
+/**
+ * @codeCoverageIgnore
+ */
+class Autoloader {
+    /** @var bool Whether the autoloader has been registered. */
+    private static $registered = false;
+
+    /**
+     * Registers PhpParser\Autoloader as an SPL autoloader.
+     *
+     * @param bool $prepend Whether to prepend the autoloader instead of appending
+     */
+    static public function register($prepend = false) {
+        if (self::$registered === true) {
+            return;
+        }
+        spl_autoload_register(array(__CLASS__, 'autoload'), true, $prepend);
+        self::$registered = true;
+    }
+
+    /**
+     * Handles autoloading of classes.
+     *
+     * @param string $class A class name.
+     */
+    static public function autoload($class) {
+        if (0 === strpos($class, 'phptojs\\')) {
+            $fileName = __DIR__ . strtr(substr($class, 9), '\\', '/') . '.php';
+            if (file_exists($fileName)) {
+                require $fileName;
+            }
+        }
+    }
+}
---
+++ lib/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+if (!class_exists('phptojs\Autoloader')) {
+    require __DIR__ . '/PhpToJs/Autoloader.php';
+}
+\phptojs\Autoloader::register();
